138ecdfcde0af93c26e055b8639f6ddd9581ead5
sethherr <seth.william.herr@gmail.com>
2014-03-23 14:35:10 -0500

    updated model lists to prevent error

--- lib/book_generator.rb
+++ lib/book_generator.rb
@@ -64,7 +64,7 @@ module BikeBook
       model_list = []      
       years.each do |year|
         puts @m_hash[manufacturer][year]
-        @m_hash[manufacturer][year].each { |bike| model_list << bike }
+        @m_hash[manufacturer][year].each { |bike| model_list << bike[:frame_model] }
       end
       model_list.uniq
     end
